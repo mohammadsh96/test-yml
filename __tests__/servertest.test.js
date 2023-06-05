2d9d05db9cb4247606bfaeafaff6bd85563e9095
mohammadsh96 <mhmd.shrydh1996@gmail.com>
2023-06-05 23:05:24 +0000

    test

--- __tests__/servertest.test.js
+++ __tests__/servertest.test.js
@@ -3,25 +3,32 @@ const server = require('../index.js');
 const supertest = require('supertest');
 const request = supertest(server.app);
 
+afterAll(() => {
+    server.close();
+});
+
 describe('myserver test', () => {
-test('getting data from home route', async () => {
-    const response = await request.get('/');
-    expect(response.status).toEqual(200);
-    expect(response.text).toEqual('Hello from home page route');
-});
-test('getting data from about route', async () => {
-    const response = await request.get('/about');
-    expect(response.status).toEqual(200);
-    expect(typeof response.body).toEqual('object');
-});
-test('getting data from bad route', async () => {
-    let num = 10;
-    num.forEach((x) => {
-        console.log(x);
+    test('getting data from home route', async () => {
+        const response = await request.get('/');
+        expect(response.status).toEqual(200);
+        expect(response.text).toEqual('Hello from home page route');
     });
-});
-test ('getting data from not found route', async () => {
-    const response = await request.get('/notfound');
-    expect(response.status).toEqual(404);
-});
+
+    test('getting data from about route', async () => {
+        const response = await request.get('/about');
+        expect(response.status).toEqual(200);
+        expect(typeof response.body).toEqual('object');
+    });
+
+    test('getting data from bad route', async () => {
+        let num = [1, 2, 3, 4, 5];
+        num.forEach((x) => {
+            console.log(x);
+        });
+    });
+
+    test('getting data from not found route', async () => {
+        const response = await request.get('/notfound');
+        expect(response.status).toEqual(404);
+    });
 });
